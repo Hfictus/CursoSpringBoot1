bb3a710756e103a4e820a3106d038cf02b3da6bd
Gerson Klauck <gk120hf@gmail.com>
2024-07-07 12:08:08 -0300

    JPA, ORM, aulajpamaven

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,5 +1,9 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -7,14 +11,33 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
+		/*Inserindo pesssoas no banco de dados:
+		 * em.getTransaction().begin();
+		 * em.persist(p1);
+		 * em.persist(p2);
+		 * em.persist(p3);
+		 * em.getTransaction().commit();
+		 **/
+		
+		//Buscar objeto no BD
+		 Pessoa p = em.find(Pessoa.class, 3);
+		 		
+		//Apagar objeto do BD:
+		 em.getTransaction().begin();
+		 em.remove(p);
+		 em.getTransaction().commit();
+		 
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 		
 		
 	}
