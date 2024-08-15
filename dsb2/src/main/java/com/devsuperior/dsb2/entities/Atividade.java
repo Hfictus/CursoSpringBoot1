967309ac8560e094d406d009d68d5873d8e8427b
Gerson Klauck <gk120hf@gmail.com>
2024-08-15 10:23:36 -0300

    Atividade, Categoria, Bloco

--- dsb2/src/main/java/com/devsuperior/dsb2/entities/Atividade.java
+++ dsb2/src/main/java/com/devsuperior/dsb2/entities/Atividade.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dsb2.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,12 +21,16 @@ public class Atividade {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	@Column(columnDefinition = "TEXT")
 	private String descricao;
 	private Double preco;
 	
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
+	
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos = new ArrayList<>();
 	
 	public Atividade() {}
 
@@ -71,8 +80,11 @@ public class Atividade {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public List<Bloco> getBlocos() {
+		return blocos;
 	};
-	
-	
+			
 	
 }
