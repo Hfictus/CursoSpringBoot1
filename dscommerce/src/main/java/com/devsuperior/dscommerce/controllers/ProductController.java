421551e6ff4c5c0092abdf93f6f131ceecbec59e
Gerson Klauck <gk120hf@gmail.com>
2024-09-06 12:27:12 -0300

    GET poducts paged

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,7 @@ import com.devsuperior.dscommerce.services.ProductService;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
-	
-	
+		
 	@Autowired
 	private ProductService service;
 	
@@ -25,4 +26,10 @@ public class ProductController {
 	/**ou:
 	 * return service.findById(id);
 	 */
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+	}
+	
 }
--- dscommerce/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -46,5 +46,4 @@ public class ProductDTO {
 	public String getImgUrl() {
 		return imgUrl;
 	}
-	
 }
