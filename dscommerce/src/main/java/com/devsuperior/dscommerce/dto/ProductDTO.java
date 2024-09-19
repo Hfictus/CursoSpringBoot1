075a7d3aa7a09939656f6d76c167f420454d807b
Gerson Klauck <gk120hf@gmail.com>
2024-09-19 18:25:58 -0300

    Bean Validation

--- dscommerce/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,22 @@ package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 	private Long id;
+	
+	@Size(min = 3, max = 80, message = "O comprimento do nome deve ter de 3 a 80 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@Size(min = 10, message = "Número mínimo de caracteres exigidos para a descrição: 10")
+	@NotBlank(message = "Campo requerido")
 	private String description;
+		
+	@Positive(message = "O valor do preço deve ser positivo")
 	private Double price;
 	private String imgUrl;
 		
--- dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -3,6 +3,6 @@ package com.devsuperior.dscommerce.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dscommerce.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
-	
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
