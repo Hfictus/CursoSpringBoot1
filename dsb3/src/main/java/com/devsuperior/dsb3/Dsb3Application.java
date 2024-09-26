0fede0217baec709cd5a2e84adf6a9ad7f9e319d
Gerson Klauck <gk120hf@gmail.com>
2024-09-26 11:06:48 -0300

    dsb3 project created

---
+++ dsb3/src/main/java/com/devsuperior/dsb3/Dsb3Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsb3Application.class, args);
+	}
+
+}
