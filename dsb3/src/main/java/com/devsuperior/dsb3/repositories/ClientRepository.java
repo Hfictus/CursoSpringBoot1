ddac9a5e0bfc141eb3103cbeb5264b6dae4a2742
Gerson Klauck <gk120hf@gmail.com>
2024-09-28 08:54:47 -0300

    ClientRepository

---
+++ dsb3/src/main/java/com/devsuperior/dsb3/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsb3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsb3.entities.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+	
+	
+	
+}
