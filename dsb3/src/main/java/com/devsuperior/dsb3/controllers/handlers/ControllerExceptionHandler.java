9b6487cc23a20a89108f664985175d3b3061805d
Gerson Klauck <gk120hf@gmail.com>
2024-10-05 12:28:53 -0300

    Exception 422

--- dsb3/src/main/java/com/devsuperior/dsb3/controllers/handlers/ControllerExceptionHandler.java
+++ dsb3/src/main/java/com/devsuperior/dsb3/controllers/handlers/ControllerExceptionHandler.java
@@ -4,22 +4,34 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.devsuperior.dsb3.dto.CustomError;
+import com.devsuperior.dsb3.dto.ValidationError;
 import com.devsuperior.dsb3.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		//CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		ValidationError err = new ValidationError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		err.addError("name", "teste 1");
+		err.addError("birthDate", "teste2");
+		return ResponseEntity.status(status).body(err);
+	}
+
 }
