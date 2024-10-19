0c9391cfff3976debaa74a6ad34cb9758e8b9b1c
Gerson Klauck <gk120hf@gmail.com>
2024-10-19 12:29:31 -0300

    controllers dto services

--- aula-salvar-para-um/src/main/java/com/devsuperior/aula/dto/PersonDepartmentDTO.java
+++ aula-salvar-para-um/src/main/java/com/devsuperior/aula/dto/PersonDepartmentDTO.java
@@ -1,5 +1,46 @@
 package com.devsuperior.aula.dto;
 
-public class PersonDepartmentDTO {
+import com.devsuperior.aula.entities.Person;
 
+public class PersonDepartmentDTO {
+	
+	private Long id;
+	private String name;
+	private Double salary;
+	
+	private DepartmentDTO department;
+
+	public PersonDepartmentDTO(Long id, String name, Double salary, DepartmentDTO department) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	public PersonDepartmentDTO(Person entity) {
+		this.id = entity.id;
+		this.name = entity.name;
+		this.salary = entity.salary;
+		this.department = entity.department;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public DepartmentDTO getDepartment() {
+		return department;
+	}
+	
+	
+	
 }
