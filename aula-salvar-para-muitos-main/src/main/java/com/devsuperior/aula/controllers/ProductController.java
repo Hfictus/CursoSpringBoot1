9a1bb6c5007d5cc7083d55f3043a2fbec6917cf4
Gerson Klauck <gk120hf@gmail.com>
2024-11-01 09:52:32 -0300

    review and aulao-nmais1

--- aula-salvar-para-muitos-main/src/main/java/com/devsuperior/aula/controllers/ProductController.java
+++ aula-salvar-para-muitos-main/src/main/java/com/devsuperior/aula/controllers/ProductController.java
@@ -2,28 +2,29 @@ package com.devsuperior.aula.controllers;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.aula.dto.ProductDTO;
+import com.devsuperior.aula.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 	
-	/*A ser feito após criação de ProductService:
-	 * @Autowired
-	 * private ProductService service;
-	 */
+	 @Autowired
+	 private ProductService service;
+	 
 	
-	//@PostMapping
+	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
-		/*A ser feito após criar ProductService:
-		 * dto = service.insert(dto);
-		 */
+		dto = service.insert(dto);
+		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		
--- aula-salvar-para-muitos-main/src/main/java/com/devsuperior/aula/services/ProductService.java
+++ aula-salvar-para-muitos-main/src/main/java/com/devsuperior/aula/services/ProductService.java
@@ -3,6 +3,11 @@ package com.devsuperior.aula.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.aula.dto.CategoryDTO;
+import com.devsuperior.aula.dto.ProductDTO;
+import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.Product;
+import com.devsuperior.aula.repositories.CategoryRepository;
 import com.devsuperior.aula.repositories.ProductRepository;
 
 @Service
@@ -10,6 +15,46 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	/*A ser usado com injeção de ProductRepository:
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category cat = new Category();
+			cat.setId(catDto.getId());
+			entity.getCategories().add(cat);			
+		}
+		
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}*/
+	
+	//insert a ser usado com injeção de CategoryRepository:
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		
+		for(CategoryDTO catDto : dto.getCategories()) {
+			
+			Category cat = categoryRepository.getReferenceById(catDto.getId()); 
+					
+			entity.getCategories().add(cat);			
+		}
+		
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}
 	
 	
 }
