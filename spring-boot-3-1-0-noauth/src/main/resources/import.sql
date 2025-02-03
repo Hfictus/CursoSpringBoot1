b7a3a8ce25268c5b479010ba70b89cd727b26d8b
Gerson Klauck <gk120hf@gmail.com>
2025-02-03 10:05:13 -0300

    BCrypt password

--- spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$2WSbrLDtamp.y/4Jpemp6.4TdexoDPMAN8ZrUA4qiMuIHYKex1JYq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$2WSbrLDtamp.y/4Jpemp6.4TdexoDPMAN8ZrUA4qiMuIHYKex1JYq');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
