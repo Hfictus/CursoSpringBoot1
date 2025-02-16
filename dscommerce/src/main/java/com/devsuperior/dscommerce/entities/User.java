168729d46f53524b85ee9739afdf8947f39bd516
Gerson Klauck <gk120hf@gmail.com>
2025-02-16 12:37:34 -0300

    Domain model User Role, seed

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -2,19 +2,22 @@ package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
-import jakarta.persistence.*;
-/*
-	import jakarta.persistence.Column;
-	import jakarta.persistence.Entity;
-	import jakarta.persistence.GeneratedValue;
-	import jakarta.persistence.GenerationType;
-	import jakarta.persistence.Id;
-	import jakarta.persistence.OneToMany;
-	import jakarta.persistence.Table;
-*/
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_user")
@@ -30,12 +33,15 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
-	//roles String[];
-	
-	
+		
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {}
 	
@@ -100,7 +106,24 @@ public class User {
 	public List<Order> getOrders() {
 		return orders;
 	}
-
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	
+	public boolean hasRole(String roleName) {
+		for(Role role : roles) {
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
