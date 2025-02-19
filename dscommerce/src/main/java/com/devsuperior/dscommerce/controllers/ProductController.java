fea5acc55160e45990a51c6c275d31faf351dcf2
Gerson Klauck <gk120hf@gmail.com>
2025-02-19 11:47:04 -0300

    Access control by user profile and by route

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,16 +35,7 @@ public class ProductController {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
-	/**Não customizado:
-	 * @GetMapping(value = "/{id}")
-	 * public ProductDTO findById(@PathVariable Long id) {
-	 *     ProductDTO dto = service.findById(id);
-	 *     return dto;
-	 * }
-	 * ou:
-	 * return service.findById(id);
-	 */
-	
+		
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(
 			@RequestParam(name = "name", defaultValue = "") String name,
@@ -51,13 +43,8 @@ public class ProductController {
 		Page<ProductDTO> dto = service.findAll(name, pageable);
 		return ResponseEntity.ok(dto);
 	}
-	/**não customizado:
-	 * @GetMapping
-	 * public Page<ProductDTO> findAll(Pageable pageable) {
-	 * 		return service.findAll(pageable);
-	 * }
-	 * */
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
@@ -65,22 +52,15 @@ public class ProductController {
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
-	/**não customizado:
-	 *@PostMapping
-	 *public ProductDTO insert(@RequestBody ProductDTO dto) {
-	 *	dto = service.insert(dto);
-	 *	return dto;
-	 *}
-	 *ou
-	 *return service.insert(dto);
-	 */
-	
+		
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
