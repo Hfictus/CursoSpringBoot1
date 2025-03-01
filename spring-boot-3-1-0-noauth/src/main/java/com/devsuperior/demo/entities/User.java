f61de0447fed4991dc5b66fc892b73314d6ec131
Gerson Klauck <gk120hf@gmail.com>
2025-03-01 07:37:57 -0300

    project without auth

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/User.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,28 +1,18 @@
 package com.devsuperior.demo.entities;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
-@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,13 +22,7 @@ public class User implements UserDetails {
     @Column(unique = true)
     private String email;
     private String password;
-    
-    @ManyToMany
-    @JoinTable(name = "tb_user_role",
-    		joinColumns = @JoinColumn(name = "user_id"),
-    		inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
-    
+
     public User() {
     }
 
@@ -81,20 +65,6 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-    public void addRole(Role role) {
-    	roles.add(role);
-    }
-    
-    public boolean hasRole(String roleName) {
-    	for(Role role : roles) {
-    		if(role.getAuthority().equals(roleName)) {
-    			return true;
-    		}
-    	}
-    	
-    	return false;
-    }
-    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -109,35 +79,4 @@ public class User implements UserDetails {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return roles;
-	}
-
-	@Override
-	public String getUsername() {
-		return email;
-	}
-	
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-	
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-		
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-	
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
-	
 }
--- spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,13 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$2WSbrLDtamp.y/4Jpemp6.4TdexoDPMAN8ZrUA4qiMuIHYKex1JYq');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$2WSbrLDtamp.y/4Jpemp6.4TdexoDPMAN8ZrUA4qiMuIHYKex1JYq');
-
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
-INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
-
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
-
